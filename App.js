d5f88b03d19870b80ecf9ce044d6e9f5d2366aa4
santhosh nama <santhosh241296@gmail.com>
2024-02-01 13:35:49 -0600

    added Files after part -3

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const ReactHeading = React.createElement('h1',{},"Hello World From React!")
 const ReactRoot=ReactDOM.createRoot(document.getElementById('root'))
 ReactRoot.render(ReactHeading);
